d87b15f614bfe747904d1ef85f61534c2827b5d3
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-07-24 11:04:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# BlockList Aggregator
+
+Inspired from https://gist.github.com/glaszig/bf96beccf4694ae25d4f1f7cc6224985 from [glaszig](https://github.com/glaszig).
+
+The script downloads all the public lists from [I-BlockList](https://www.iblocklist.com/lists) into a super list.
+
+## Release
+
+Every day the script is executed by Github Actions and updates the `nightly` tag.
+
+You can download the file directly here:
+https://github.com/GreatWizard/blockp2p/releases/download/nightly/blockp2p.list.gz
---
+++ index.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# Version 2, December 2004
+#
+# Copyright (C) 2020 glaszig <glaszig@gmail.com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+# inspired by https://gist.github.com/johntyree/3331662#gistcomment-1968023
+#
+# usage:
+#  UA="user agent string" mkp2pblocklist > /tmp/blockp2p.list
+# or
+#  UA="user agent string" mkp2pblocklist | gzip -9 > /tmp/blockp2p.list.gz
+
+PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PATH"
+
+if [ -z "$UA" ]; then
+    echo "User Agent environment variable UA missing."
+    echo "See, e.g., https://user-agents.net/browsers for examples."
+    exit 1
+fi
+
+curl -A "$UA" -s https://www.iblocklist.com/lists.php |
+    sed -n "s/.*value='\(http:.*=p2p.*\)'.*/\1/p" |
+    xargs curl -A "$UA" -sL |
+    gunzip |
+    egrep -v '^#' |
+    uniq
